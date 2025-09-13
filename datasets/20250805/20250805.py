763fdc7bfc2643bbc6ca3ac1a977863fc74c23a3
serban <serban.dobroiu@gmail.com>
2025-09-13 12:35:01 +0300

    initial commit

---
+++ datasets/20250805/20250805.py
@@ -0,0 +1,4 @@
+import polars as pl
+
+# raw_data: pl.DataFrame = pl.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-08-05/income_inequality_raw.csv")
+raw_data = pl.read_csv("income_inequality_raw.csv")
